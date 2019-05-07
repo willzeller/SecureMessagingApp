23f6017ff1314f7971554fdb625ac16e76b3242d
kgearhar <kgears502@gmail.com>
2019-05-06 19:39:09 -0600

    now with sparkly ass guis

---
+++ SecureMessagingApp/FindFriend.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SecureMessagingApp
+{
+    partial class FindFriend
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.friendIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.connectFriend = new System.Windows.Forms.Button();
+            this.ConnectionMessages = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // friendIP
+            // 
+            this.friendIP.Location = new System.Drawing.Point(22, 30);
+            this.friendIP.Multiline = true;
+            this.friendIP.Name = "friendIP";
+            this.friendIP.Size = new System.Drawing.Size(217, 26);
+            this.friendIP.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Friend\'s IP address:";
+            // 
+            // connectFriend
+            // 
+            this.connectFriend.Location = new System.Drawing.Point(273, 15);
+            this.connectFriend.Name = "connectFriend";
+            this.connectFriend.Size = new System.Drawing.Size(204, 41);
+            this.connectFriend.TabIndex = 2;
+            this.connectFriend.Text = "Open Connection";
+            this.connectFriend.UseVisualStyleBackColor = true;
+            // 
+            // ConnectionMessages
+            // 
+            this.ConnectionMessages.Location = new System.Drawing.Point(273, 94);
+            this.ConnectionMessages.Multiline = true;
+            this.ConnectionMessages.Name = "ConnectionMessages";
+            this.ConnectionMessages.ReadOnly = true;
+            this.ConnectionMessages.Size = new System.Drawing.Size(204, 72);
+            this.ConnectionMessages.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Connection Status";
+            // 
+            // FindFriend
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 194);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ConnectionMessages);
+            this.Controls.Add(this.connectFriend);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.friendIP);
+            this.Name = "FindFriend";
+            this.Text = "FindFriend";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox friendIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button connectFriend;
+        private System.Windows.Forms.TextBox ConnectionMessages;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ SecureMessagingApp/MessagingGUI.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SecureMessagingApp
+{
+    partial class MessagingGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MessagingGUI));
+            this.friendMessage = new System.Windows.Forms.TextBox();
+            this.myMessage = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // friendMessage
+            // 
+            this.friendMessage.Location = new System.Drawing.Point(28, 26);
+            this.friendMessage.Multiline = true;
+            this.friendMessage.Name = "friendMessage";
+            this.friendMessage.ReadOnly = true;
+            this.friendMessage.Size = new System.Drawing.Size(381, 183);
+            this.friendMessage.TabIndex = 0;
+            // 
+            // myMessage
+            // 
+            this.myMessage.Location = new System.Drawing.Point(28, 242);
+            this.myMessage.Multiline = true;
+            this.myMessage.Name = "myMessage";
+            this.myMessage.Size = new System.Drawing.Size(381, 135);
+            this.myMessage.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(526, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(426, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(257, 196);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MessagingGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 401);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.myMessage);
+            this.Controls.Add(this.friendMessage);
+            this.Name = "MessagingGUI";
+            this.Text = "OMG Geareller Messaging";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox friendMessage;
+        private System.Windows.Forms.TextBox myMessage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+    }
+}
